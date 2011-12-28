de5b727d9036a9f7198bb8ee66c3d1e376a07354
John Grubb <therealjohnnygrubb@gmail.com>
2011-12-28 06:51:00 -0800

    preparing for upgrades

--- sites/all/themes/pr11/templates/page.tpl.php
+++ sites/all/themes/pr11/templates/page.tpl.php
@@ -14,9 +14,20 @@
 <!--[if lte IE 7]>
     <style type="text/css" media="all">@import "<?php print base_path() . path_to_theme() ?>/lte_ie7.css";</style>
   <![endif]-->
+<script type="text/javascript">
 
-  </head>
-  <body <?php print phptemplate_body_attributes($is_front, $layout); ?> <?php // print drupal_attributes($attr) ?>>
+  var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-23178332-1']);
+    _gaq.push(['_trackPageview']);
+      (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+</script>
+</head>
+<body <?php print phptemplate_body_attributes($is_front, $layout); ?> <?php // print drupal_attributes($attr) ?>>
   
 <div id="prContainer">
 
