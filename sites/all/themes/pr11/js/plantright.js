38dc2be87e3730dfafef97a6a4ba3e0db3b3495c
John Grubb <therealjohnnygrubb@gmail.com>
2013-01-28 06:52:57 -0500

    Javascript for pledge agreement

--- sites/all/themes/pr11/js/plantright.js
+++ sites/all/themes/pr11/js/plantright.js
@@ -21,6 +21,9 @@ Drupal.behaviors.tao = function (context) {
       return false;
     });
   });
+  $('body#page-node-add-business input#edit-title').change(function(){
+    $('span.nursery-name').text($(this).val()); 
+ });
 };
 
 jQuery(document).ready(function($) {
@@ -60,8 +63,4 @@ jQuery(document).ready(function($) {
     $('#tab-box').height(boxHeight);
   });
   $('#block-admin-account li:eq(1)').remove();
-  $('#page-node-add-business input#edit-title').change(function(){
-    $('span.nursery-name').text($(this).val()); 
-    console.log($(this).val());
-  });
 });
