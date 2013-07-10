5c54c8d0c44700380f0226e99754e4ebe0023919
Cristina Hanes <cristinawithout@gmail.com>
2013-07-10 13:07:03 -0700

    GA event tracking new window

--- sites/all/themes/pr11/js/ga-events.js
+++ sites/all/themes/pr11/js/ga-events.js
@@ -4,17 +4,21 @@
  */
 
 jQuery(document).ready(function($) {
-  var filetypes = /\.(zip|exe|pdf|doc*|xls*|ppt*|mp3)$/i;
+  var filetypes = /\.(rtf|txt|zip|exe|pdf|doc.?|xl.{0,2}|ppt.{0,2}|mp3)$/i;
   var baseHref = 'http://' + document.domain;
 
   $('a').each(function() {
     var href = $(this).attr('href');
     if (href && (href.match(/^https?\:/i)) && (!href.match(document.domain))) {
+      $(this).click(function(e) {
+        e.preventDefault();
+      });
       $(this).mousedown(function(e) {
         e.preventDefault();
+        e.stopPropagation();
         var extLink = href.replace(/^https?\:\/\//i, '');
-        _gaq.push(['_trackEvent', 'External', 'Click', extLink]);
         window.open(href);
+        _gaq.push(['_trackEvent', 'External', 'Click', extLink]);
       });
     }
     else if (href && href.match(/^mailto\:/i)) {
@@ -24,12 +28,16 @@ jQuery(document).ready(function($) {
       });
     }
     else if (href && href.match(filetypes)) {
+      $(this).click(function(e) {
+        e.preventDefault();
+      });
       $(this).mousedown(function(e) {
+        e.stopPropagation();
         e.preventDefault();
         var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined;
         var filePath = href;
-        _gaq.push(['_trackEvent', 'Download', extension, filePath]);
         window.open(href);
+        _gaq.push(['_trackEvent', 'Download', extension, filePath]);
       });
     }
   });
