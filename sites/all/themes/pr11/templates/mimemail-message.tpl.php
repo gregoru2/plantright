ffd85662a20d1ed489668a90e15cd89187b9c61c
Cristina Hanes <cristinawithout@705216.no-reply.drupal.org>
2013-12-15 22:12:49 -0800

    Re-change mail template

--- sites/all/themes/pr11/templates/mimemail-message.tpl.php
+++ sites/all/themes/pr11/templates/mimemail-message.tpl.php
@@ -32,7 +32,7 @@ $path = $GLOBALS['base_url'] . '/images';
   </head>
   <body id="mimemail-body" <?php if ($mailkey): print 'class="'. $mailkey .'"'; endif; ?> style="margin: 10px auto; text-align: center;">
 <table align="center" border="0" cellpadding="0" cellspacing="0" style="border: 0px; border-collapse: collapse; width: 740px; font-size: 15px; line-height: 21px;">
-<tr><td align="center" style="padding: 10px;"><img src="<?php print $path; ?>/plantright_partner_logo.png" height="123" width="96" alt="PlantRight Partner logo"></td></tr></table>
+<tr><td align="center" style="padding: 10px;"><img src="<?php print $path; ?>/logo_no_tagline.png" height="135" width="140" alt="PlantRight logo"></td></tr></table>
 <table align="center" cellpadding="0" cellspacing="0" style="border-top: 7px solid #bcd030; border-left: 2px solid #bcd030; border-right: 2px solid #bcd030; border-bottom: 12px solid #6a5834; border-collapse: collapse; width: 740px; font-size: 15px; line-height: 21px;">
 <tr><td style="text-align: left; padding: 30px 53px; font-family: Helvetica, Arial, sans-serif; font-size: 15px; line-height: 21px; color: #707070;">
 <?php print $body; ?>
