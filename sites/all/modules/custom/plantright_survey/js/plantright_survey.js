bd9a5e8efbaca0bcaeda5b2e2089b07bf31e2bb1
Cristina Hanes <cristinawithout@705216.no-reply.drupal.org>
2014-01-13 23:13:50 -0800

    Customizations to the survey data submission node form

--- sites/all/modules/custom/plantright_survey/js/plantright_survey.js
+++ sites/all/modules/custom/plantright_survey/js/plantright_survey.js
@@ -1,8 +1,8 @@
 Drupal.behaviors.plantright_survey = function (context) {
-  $('#page-node-add-survey-photos input.form-radio').click(function() {
-    console.log(this);
+  $('#page-node-add-survey-photos input.form-radio, #page-node-add-survey-submission input.form-radio, ').click(function() {
     $code = $(this).siblings('span.views-field-field-store-code-value').children('.field-content').html();
     $('input#edit-title').val($code);
   });
+  $('#page-node-add-survey-submission .date-month label, #page-node-add-survey-submission .date-day label, #page-node-add-survey-submission .date-year label,  #page-node-add-survey-photos .views-field-field-store-code-value, #page-node-add-survey-submission .views-field-field-store-code-value').hide();
   $('#page-node-add-survey-photos #edit-field-survey-image-field-survey-image-add-more').val('Add another photo');
 }
