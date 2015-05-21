230f63b007b922ea66783e3f4cac3aeea49c9787
cristina <cristinawithout@gmail.com>
2015-05-20 22:45:05 -0700

    Add check for edit perm on max locations prompt. Add 225 as location count option

--- sites/all/modules/custom/plantright_business_directory/js/plantright_business_directory.js
+++ sites/all/modules/custom/plantright_business_directory/js/plantright_business_directory.js
@@ -34,13 +34,26 @@ Drupal.behaviors.plantright_bus_dir = function(context) {
 
         var $input = $('<input type="button" id="pbd-location-add-another" class="form-submit" value="Add another location" />')
         .click(function(e) {
+          if (location_count >= 100) {
+            $('#location-count-warning').remove();
+            $input.after('<p id="location-count-warning">You have 100 or more locations. You may experience errors attempting to save. Please contact an administrator.</p>');
+          }
+
+
           if ($('fieldset.location:nth-child(' + (location_count + 1) + ')').length > 0) {
             $('fieldset.location:nth-child(' + (location_count + 1) + ')').show();
             location_count++;
-          } else if (location_count < 300) {
-            $input.after('<p>You have reached the maximum number of locations that can be entered at one time. Please save and edit again to add more.</p>');
+          }
+          else if (location_count < 300) {
+            if (Drupal.settings.plantright_business_directory.canEditBusiness) {
+              $input.after('<p>You have reached the maximum number of locations that can be entered at one time. Please save and edit again to add more.</p>');
+            }
+            else {
+              $input.after('<p>You have reached the maximum number of locations that can be entered. Please contact us to add more.</p>');
+            }
             $input.remove();
-          } else {
+          }
+          else {
             $input.after('<p>You have reached the maximum number of locations.</p>');
             $input.remove();
           }
