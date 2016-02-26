8ca0283298785f4f98cfde1c7bc48c59d2da274e
cristina <cristinawithout@gmail.com>
2016-02-25 22:36:00 -0800

    Validation and messages for the photo upload

--- sites/all/modules/custom/plantright_survey/js/plantright_survey.js
+++ sites/all/modules/custom/plantright_survey/js/plantright_survey.js
@@ -79,15 +79,15 @@ Drupal.behaviors.plantright_survey = function (context) {
   // Not using the Drupal context, because context won't include the full form when images change.
   var $photoForm = $(survey_photo_selector);
   var $imgsTable = $photoForm.find('#field_survey_image_values');
-  
+
   // Change header.
   $imgsTable.find('thead').hide();
   $imgsTable.before('<div class="form-item">\n\
     <label>Photos: <span class="form-required" title="This field is required.">*</span></label>\n\
     <p class="survey-photo-upload-note">Click the button below to upload your photos and provide descriptions. In the description field, please include the plant name and specify if the photo is of the plant itself or the plant\'s tag (e.g.: Pampas grass, tag).</p>\n\
-    <p class="survey-photo-issues-note">If you encounter errors submitting your photos, please send your photos and descriptions to Chris Crawford at <a href="mailto:ccrawford@suscon.org">ccrawford@suscon.org</a>. Thank you!</p>\n\
+    <p class="survey-photo-issues-note">Please note that you may now upload photos directly from your mobile devices (including iOS and Android devices). Please wait for all photos to load before clicking “Submit.” If you encounter errors submitting your photos, please contact <a href="mailto:PlantRight@suscon.org">PlantRight@suscon.org</a>. Thank you!</p>\n\
   </div>');
-  
+
   // Hide all rows with file inputs to fix weird behavior of imagefield zip that shows empty file inputs.
   // (Use file inputs because ones that are multi upload have an image not input
   $imgsTable.find('tbody tr').each(function(index, val) {
@@ -102,6 +102,11 @@ Drupal.behaviors.plantright_survey = function (context) {
       var $descInput = $row.find('input[type=text]');
       if ($imgPreview.length > 0 && $descInput.length > 0 && $descInput.val() === '') {
         var desc = $imgPreview.attr('title')
+        if (!desc || desc === '') {
+          // Title is empty. Use src.
+          desc = $imgPreview.attr('src');
+          desc = desc.substring(desc.lastIndexOf('/') + 1);
+        }
         // Remove file extension from desc.
         desc = desc.replace(/\.[^.]+$/, '');
         $descInput.val(desc);
@@ -111,13 +116,17 @@ Drupal.behaviors.plantright_survey = function (context) {
 
   // Hide the add more button right after the images table.
   $photoForm.find('#field_survey_image_values + .content-add-more').hide();
-  
+
   // Hide the "Multiple image label"
   $photoForm.find('label[for=edit-field-survey-image-upload]').hide();
-  
+
   // Hide the "Start Upload" button because we're auto starting on input change.
   $photoForm.find('.zip-field input[type=submit]').hide();
   
+  // Executed each time the files change. So resetting the file input to show and removing any throbbers.
+  $photoForm.find('.zip-field input[type=file]').show();
+  $photoForm.find('.zip-field .ahah-progress, .zip-field .throbber').remove();
+
   // Listen for the zip field input change in order to automatically start the upload
   // Because imagefield makes users click the start button
   $photoForm.find('.zip-field input[type=file]').unbind('change.pr-upload-change').bind('change.pr-upload-change', function() {
@@ -126,11 +135,51 @@ Drupal.behaviors.plantright_survey = function (context) {
     if (!$btn.hasClass('pr-btn-clicked')) {
       // Don't call mousedown twice within 200 milliseconds
       $btn.trigger('mousedown').addClass('pr-btn-clicked');
+      $photoForm.find('.zip-field input[type=file]').hide();
       setTimeout(function() {
         $btn.removeClass('pr-btn-clicked');
       }, 200);
     }
   });
   // END IMAGEFIELD ZIP UPLOAD CUSTOMIZATIONS
+  
+  // Let them know max size applies to all.
+  var $sizeDesc = $('.zip-field .zip-description');
+  var desc = $sizeDesc.html().replace('Maximum filesize', '<br/>Maximum size per photo batch');
+  desc += '. (You may upload in multiple batches if the total of all your images are larger than 16MB.)';
+  $sizeDesc.html(desc);
+
+  // Disable submit until images are uploaded 
+  var throbber = '<div class="ahah-progress ahah-progress-throbber"><div class="throbber">&nbsp;</div></div>';
+  var triesLeft = 120;
+  $photoForm.find('input[type=submit][value=Submit]').bind('mousedown', function(e) {
+    var $button = $(this);
+    $photoForm.find('#pr-survey-js-valid-msg').remove();
+    var validFail = false;
+    if ($photoForm.find('.zip-field .throbber').length > 0) {
+      validFail = throbber + ' Submitting... Your images are still uploading. Large files may time some time to complete.<br/> Please do not navigate away from this page. (You may change to another application and return.)';
+      if (triesLeft > 0) {
+        triesLeft--;
+        setTimeout(function() {
+          $button.trigger('mousedown');
+        }, 1000);
+      }
+    }
+    
+    if (!validFail && $photoForm.find('#field-survey-image-items .error').length > 0) {
+      validFail = 'There was a problem uploading one or more of your photos';
+    }
+    
+    if (!validFail && $photoForm.find('#field_survey_image_values .imagefield-preview').length < 1) {
+      validFail = 'You have not uploaded any photos';
+    }
+
+    if (validFail) {
+      $button.before('<label id="pr-survey-js-valid-msg" class="error"><strong>' + validFail + '</strong></label>');
+      e.stopPropagation();
+      e.preventDefault();
+      return false;
+    }
+  });
 
 }
